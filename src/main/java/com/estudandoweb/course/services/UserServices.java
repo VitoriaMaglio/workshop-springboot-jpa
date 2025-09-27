111268d84f3c43bb2790ee64d9541170ddb447c9
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-09-27 14:24:16 -0300

    User insert.

--- src/main/java/com/estudandoweb/course/services/UserServices.java
+++ src/main/java/com/estudandoweb/course/services/UserServices.java
@@ -2,6 +2,7 @@ package com.estudandoweb.course.services;
 
 import com.estudandoweb.course.entities.User;
 import com.estudandoweb.course.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import java.util.Optional;
 
 //essa camada repassa as ações para camada repositories
 @Service
+@Transactional
 public class UserServices  {
     @Autowired //indica que tem uma dependência à camada repositories de acesso ao banco
     private UserRepository userRepository;
@@ -21,6 +23,10 @@ public class UserServices  {
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
         return obj.get();
+    }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);//inserindo no banco de dados um novo objeto usuário
     }
 
 
