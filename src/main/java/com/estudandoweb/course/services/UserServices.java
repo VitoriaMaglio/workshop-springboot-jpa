ec328fcf52973084c257291e0d860042f1c590b3
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-09-27 15:01:52 -0300

    Exception handlig delete and update.

--- src/main/java/com/estudandoweb/course/services/UserServices.java
+++ src/main/java/com/estudandoweb/course/services/UserServices.java
@@ -2,10 +2,14 @@ package com.estudandoweb.course.services;
 
 import com.estudandoweb.course.entities.User;
 import com.estudandoweb.course.repositories.UserRepository;
+import com.estudandoweb.course.services.exceptions.DatabaseException;
 import com.estudandoweb.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,13 +35,23 @@ public class UserServices  {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);//deixa um obj monitorado pelo JPA
-        updateData(entity, obj);
-        return userRepository.save(entity);//efeuta ação no banco
+        try {
+            User entity =  userRepository.getOne(id);
+            updateData(entity, obj);
+            return  userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
